ef2029571a8b1a3c7ab1541f86428d16fe5ad9b1
Asesci <76848312+Asesci@users.noreply.github.com>
2022-09-11 14:58:33 +0300

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
-length = float(input("Your length (in cm): "))
+height = float(input("Your height (in cm): "))
 weight = float(input("Your weight (in kg): "))
 
-length = length / 100
-bmi = weight / length ** 2
+height = height / 100
+bmi = weight / height ** 2
 
 print(f"Your BMI is {bmi}")
